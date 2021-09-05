2e94d58d51afa80f503e46caf0209dbb5c479a16
Jaguellz <jaguellz@outlook.com>
2021-09-05 23:36:05 +0300

    done

--- ReverseTest.php
+++ ReverseTest.php
@@ -1,10 +1,24 @@
 <?php
 use PHPUnit\Framework\TestCase;
+require 'index.php';
 
 class ReverseTest extends TestCase
 {
+    private $reverser;
+
+    protected function setUp() : void
+    {
+        $this->reverser = new Reverser();
+    }
+
+    protected function tearDown() : void
+    {
+        $this->reverser = NULL;
+    }
+
     public function testRevert()
     {
-
+        $result = $this->reverser->revertCharacters('Hello, World!');
+        $this->assertEquals('Olleh, Dlrow!', $result);
     }
 }
--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-function revertCharacters($string){
-    $string_reversed = explode(' ', $string);
-    foreach ($string_reversed as & $item){
-        $item = str_split($item);
-        $uppers = [];
-        foreach ($item as $key => & $value){
-            if (ctype_upper($value)) array_push($uppers, $key);
+class Reverser
+{
+    public function revertCharacters($string)
+    {
+        $string_reversed = explode(' ', $string);
+        foreach ($string_reversed as & $item) {
+            $item = str_split($item);
+            $uppers = [];
+            foreach ($item as $key => & $value) {
+                if (ctype_upper($value)) array_push($uppers, $key);
+            }
+            $item = implode($item);
+            $item = preg_replace_callback('/\w+/', function($found) {
+                foreach ($found as $item) {
+                    return strrev($item);
+                }
+            }, $item);
+            $item = str_split($item);
+            foreach ($item as $key => & $value) {
+                if (in_array($key, $uppers)) $value = strtoupper($value);
+                else $value = strtolower($value);
+            }
+            $item = implode($item);
         }
-        $item = array_reverse($item);
-        foreach ($item as $key => & $value) {
-            if (in_array($key, $uppers)) $value = strtoupper($value);
-            else $value = strtolower($value);
-        }
-        $item = implode($item);
+        $string_reversed = join(' ', $string_reversed);
+        return $string_reversed;
     }
-    $string_reversed = join(' ', $string_reversed);
-    return $string_reversed;
 }
-
-$str = 'zxc';
-echo revertCharacters($str);
