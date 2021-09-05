322cc2a44a1fff2870e0bf70f529d63d00dfb108
Jaguellz <jaguellz@outlook.com>
2021-09-05 22:18:38 +0300

    done main code

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 function revertCharacters($string){
     $string_reversed = explode(' ', $string);
-    foreach ($string_reversed as $item){
+    foreach ($string_reversed as & $item){
         $item = str_split($item);
-
+        $uppers = [];
+        foreach ($item as $key => & $value){
+            if (ctype_upper($value)) array_push($uppers, $key);
+        }
+        $item = array_reverse($item);
+        foreach ($item as $key => & $value) {
+            if (in_array($key, $uppers)) $value = strtoupper($value);
+            else $value = strtolower($value);
+        }
+        $item = implode($item);
     }
-    return 0;
+    $string_reversed = join(' ', $string_reversed);
+    return $string_reversed;
 }
+
+$str = 'zxc';
+echo revertCharacters($str);
