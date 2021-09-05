611de2bd4f6941026c3d5e47d6e91c7b715e2d61
Jaguellz <jaguellz@outlook.com>
2021-09-05 21:42:22 +0300

    initial commit

---
+++ ReverseTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ReverseTest extends TestCase
+{
+    public function testRevert()
+    {
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+function revertCharacters($string){
+    $string_reversed = explode(' ', $string);
+    foreach ($string_reversed as $item){
+        $item = str_split($item);
+
+    }
+    return 0;
+}
